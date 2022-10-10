40b5bdf384f1dd9a2b9a7ec4b9dc733d31c3d82b
Aminat-ola121 <Olajuwonaminat121@gmail.com>
2022-10-10 09:57:30 -0700

    update

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,12 +1,5 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define FUNCTION_LIKE_MACRO_H
+#ifndef ABS_DEF
+#define ABS_DEF
+#define ABS(x) (x > 0 ? (x) : (x) * -1)
+#endif /* end ABS_DEF */
 
-/*
- * File: 3-function_like_macro.h
- * Desc: Header file that defines a function-like macro ABS(x)
- *       that computes the absolute value of a number x.
- */
-
-#define ABS(x) ((x) < 0 ? (-x) : (x))
-
-#endif
